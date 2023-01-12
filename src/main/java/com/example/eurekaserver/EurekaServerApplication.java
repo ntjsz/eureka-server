8c64625b161daac23fbed070acb4c42cfcf3d377
huhaitao <huhaitao@tianyancha.com>
2023-01-12 19:06:03 +0800

    sentinel-1

--- src/main/java/com/example/eurekaserver/EurekaServerApplication.java
+++ src/main/java/com/example/eurekaserver/EurekaServerApplication.java
@@ -3,10 +3,11 @@ package com.example.eurekaserver;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaServer
+@EnableEurekaClient
 public class EurekaServerApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/eurekaserver/controller/ServiceInstanceRestController.java
+++ src/main/java/com/example/eurekaserver/controller/ServiceInstanceRestController.java
@@ -4,12 +4,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+
 
 @RestController
 public class ServiceInstanceRestController {
 
     @GetMapping("/echo")
     public String echo(@RequestParam("hi") String content) {
+        System.out.println(LocalDateTime.now() + "   " + content);
         return content;
     }
 }
